d2238f583348dabbf3e32c1bb9948c0b58b6eaef
Pablo Levin <plevin@technisys.com>
2021-10-21 20:53:21 -0300

    loader

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -38,15 +38,17 @@ function ItemList() {
 					/>
 				))
 			) : (
-				<div class='loader'>
-					<div></div>
-					<div></div>
-					<div></div>
-					<div></div>
-					<div></div>
-					<div></div>
-					<div></div>
-					<div></div>
+				<div className='loader-container'>
+					<div className='loader'>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
 				</div>
 			)}
 		</>
